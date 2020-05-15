9817871578102ec52fce08db7f9d38931fff8362
Ben Hale <bhale@vmware.com>
2020-05-15 06:08:52 -0700

    Merge RFC Script
    This change creates a script used for merging approved RFCs.  This script
    takes a PR number as an argument, checks out the branch, merges it to master,
    moves the file to the next numbered RFC and updates the RFC with a link back
    to the PR it was from.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

---
+++ merge-rfc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+set -eu
+
+id() {
+  ID=$(find text -depth 1 | sed -E 's|^text/([[:digit:]]{4})-.*$|\1|' | sort | tail -n 1)
+  ((ID++))
+  printf "%04d" "${ID}"
+}
+
+if [[ $# != 1 ]]; then
+  printf "Usage: %s <PR#>\n" "${0:t}"
+  exit 1
+fi
+
+git pull origin --rebase
+
+PR="${1}"
+ID=$(id)
+
+git fetch origin "pull/${PR}/head:rfc-${ID}"
+git merge "rfc-${ID}" --signoff --no-edit --no-ff
+git branch -d "rfc-${ID}"
+
+SOURCE=$(find text -depth 1 -name '0000-*')
+TARGET=${SOURCE//0000/$(printf "%04d" "${ID}")}
+
+sed -i '' "s|- RFC Pull Request: (leave blank)|- RFC Pull Request: [rfcs#${PR}](https://github.com/buildpacks/rfcs/pull/${PR})|" "${SOURCE}"
+git mv "${SOURCE}" "${TARGET}"
+git add "${TARGET}"
+git commit --signoff --message "RFC #${ID}
+
+[#${PR}]
+"
